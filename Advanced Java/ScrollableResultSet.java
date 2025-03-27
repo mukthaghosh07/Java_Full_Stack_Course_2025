eef7326daed5eed9ff5c788609f0e86584ab7d43
Muktha Ghosh <129301870+mukthaghosh07@users.noreply.github.com>
2025-03-27 10:04:39 +0530

    Add files via upload

---
+++ Advanced Java/ScrollableResultSet.java
@@ -0,0 +1,65 @@
+package firstprog;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class ScrollableResultSet {
+
+public static void main(String arr[])throws Exception
+{
+
+Statement st;
+Connection con;
+ResultSet rs;
+//Class.forName("com.mysql.jdbc.Driver");
+//System.out.println("driver loaded and registered");
+Connection con1 = DriverManager.getConnection("jdbc:mysql://localhost:3306/jfs", "root", "amp12345");
+System.out.println("Connection established");
+st=con1.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE,ResultSet.CONCUR_UPDATABLE);
+rs=st.executeQuery("select * from company");
+
+
+rs.first();
+int i=rs.getInt(1);
+String ss=rs.getString(2);
+System.out.println("id no is"+i);
+System.out.println("name is"+ss);
+
+
+rs.absolute(3);
+int ii=rs.getInt(1);
+String s=rs.getString(2);
+System.out.println("id no is"+ii);
+System.out.println("name is"+s);
+
+
+rs.previous();
+int ij1=rs.getInt(1);
+String sss1=rs.getString(2);
+System.out.println("id no is"+ij1);
+System.out.println("name is"+sss1);
+
+
+rs.next();
+int ij=rs.getInt(1);
+String sss=rs.getString(2);
+System.out.println("id no is"+ij);
+System.out.println("name is"+sss);
+
+
+rs.last();
+int ij2=rs.getInt(1);
+String sss2=rs.getString(2);
+System.out.println("id no is"+ij2);
+System.out.println("name is"+sss2);
+
+
+
+rs.close();
+st.close();
+con1.close();
+
+
+}}
---
+++ Advanced Java/batch_update.java
@@ -0,0 +1,34 @@
+package firstprog;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.util.*;
+
+public class batch_update {
+	
+	public static void main(String[] args) throws Exception {
+		//Class.forName("com.mysql.jdbc.Driver");
+		System.out.println("driver loaded");
+		
+		Connection con1 = DriverManager.getConnection("jdbc:mysql://localhost:3306/jfs", "root", "amp12345");
+		System.out.println("conncetion is created");
+		
+	Statement st=con1.createStatement();
+	st.addBatch("insert into company values(107,'wipro',484878)");
+	st.addBatch("insert into company values(108,'amazon',5465346)");
+	st.addBatch("insert into company values(109,'Samsung',63622)");
+	st.addBatch("insert into company values(110,'Goldmach sachs',36562)");
+	st.addBatch("insert into company values(111,'hcl',536536)");
+	
+	
+	int arr1[]=st.executeBatch();
+	System.out.println(" Number of row updated is: "+arr1.length);
+	
+		
+	st.close();
+	con1.close();
+	
+	}
+
+}
